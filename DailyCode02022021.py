345e305167670075cac2f826abb158ab36fd9023
Banehowl <76931164+Banehowl@users.noreply.github.com>
2021-02-02 08:35:53 -0800

    Maximum Range of a Triangle
    Daily Code for 02/02/2021

---
+++ DailyCode02022021.py
@@ -0,0 +1,19 @@
+# -------------------------------------------------------
+#	Daily Code	02/02/2021
+#   "Maximum Edge of a Triangle" Lesson from edabit.com
+#   Coded by: Banehowl
+# -------------------------------------------------------
+
+# Create a function that finds the maximum range of a triangle's third edge, where the side lengths are all integers.
+
+# next_edge(8, 10) -> 17
+# next_edge(5, 7) -> 11
+# next_edge(9, 2) -> 10
+
+def next_edge(side1, side2):
+    side3 = (side1 + side2) - 1
+    return(side3)
+
+print next_edge(8, 10)
+print next_edge(5, 7)
+print next_edge(9, 2)
