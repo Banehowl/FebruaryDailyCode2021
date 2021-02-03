35abfb70dc508b46ee2191908973c8d7dc184d73
Banehowl <76931164+Banehowl@users.noreply.github.com>
2021-02-03 08:36:34 -0800

    Return a String as an Integer
    Daily Code on 02/03/2021

---
+++ DailyCode02032021.py
@@ -0,0 +1,15 @@
+# ----------------------------------------------------------
+#	Daily Code	02/03/2021
+#   "Return a String as an Integer" Lesson from edabit.com
+#   Coded by: Banehowl
+# ----------------------------------------------------------
+
+# Create a function that takes a string and returns it as an integer
+# string_int("6") -> 6
+# string_int("1000") -> 1000
+# string_int("12") -> 12
+
+def string_int(txt):
+    integer = int(txt)
+    return(integer)
+
