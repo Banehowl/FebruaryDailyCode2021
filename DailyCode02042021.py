e91929d7ad3f9086b2a35545bba92eb5d77b3304
Banehowl <76931164+Banehowl@users.noreply.github.com>
2021-02-04 08:50:39 -0800

    Update DailyCode02042021.py

--- DailyCode02042021.py
+++ DailyCode02042021.py
@@ -6,13 +6,13 @@
 
 # Create a function that takes the age and return the age in days
 # calc_age(65) -> 23725
-# calc_age(65) -> 0
-# calc_age(65) -> 7300
+# calc_age(0) -> 0
+# calc_age(20) -> 7300
 
 def calc_age(age):
     days = age * 365
     return(days)
 
 print calc_age(65)
-print calc_age(65)
-print calc_age(65)
+print calc_age(0)
+print calc_age(20)
