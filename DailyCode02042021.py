43f18a82db948f9750865b3fb06265746ee4a983
Banehowl <76931164+Banehowl@users.noreply.github.com>
2021-02-04 08:49:02 -0800

    Convert Age to Days
    Daily Code for 02/04

---
+++ DailyCode02042021.py
@@ -0,0 +1,18 @@
+# ------------------------------------------------
+#	Daily Code	02/04/2021
+#   "Convert Age to Days" Lesson from edabit.com
+#   Coded by: Banehowl
+# ------------------------------------------------
+
+# Create a function that takes the age and return the age in days
+# calc_age(65) -> 23725
+# calc_age(65) -> 0
+# calc_age(65) -> 7300
+
+def calc_age(age):
+    days = age * 365
+    return(days)
+
+print calc_age(65)
+print calc_age(65)
+print calc_age(65)
