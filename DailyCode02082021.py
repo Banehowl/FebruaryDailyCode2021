93a60818d8730828fdd87b3591f6ff77a0212dc8
Banehowl <76931164+Banehowl@users.noreply.github.com>
2021-02-08 08:37:09 -0800

    Sum of Polygon ANgles
    Daily Code on 02/08/2021

---
+++ DailyCode02082021.py
@@ -0,0 +1,14 @@
+# --------------------------------------------------
+# #	Daily Code	02/08/2021
+#   "Sum of Polygon Angles" Lesson from edabit.com
+#   Coded by: Banehowl
+# --------------------------------------------------
+
+# Given an n-sided regular polygon n, return the total sum of the internal angles (in degrees)
+# sum_polygon(3) -> 180
+# sum_polygon(4) -> 360
+# sum_polygon(6) -> 720
+
+def sum_polygon(n):
+        solution = (n-2) * 180
+        return(solution)
