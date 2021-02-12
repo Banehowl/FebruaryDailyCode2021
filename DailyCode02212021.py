65e9254a8992aaafc8efc2fb743b1e313d476913
Banehowl <76931164+Banehowl@users.noreply.github.com>
2021-02-12 06:30:33 -0800

    Correct the Mistakes
    Daily Code for 02/12/2021

---
+++ DailyCode02212021.py
@@ -0,0 +1,24 @@
+# -------------------------------------------------
+# #	Daily Code	02/12/2021
+#   "Correct the Mistakes" Lesson from edabit.com
+#   Coded by: Banehowl
+# -------------------------------------------------
+
+# Fix the code in the code tab to pass this challenge (only syntax errors).
+# Look at the examples below to get an idea of what the function should do.
+
+# squared(5) -> 25
+# squared(9) -> 81
+# squared(100) -> 10000
+
+# Code to fix:
+# def squaed(b):
+#   return a * a
+
+def squared(a):
+    solution = a * a
+    return(solution)
+
+print squared(5)
+print squared(9)
+print squared(100)
