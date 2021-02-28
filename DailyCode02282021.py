ddee9fbfea5253b20d308e27d73552c61c413b6e
Banehowl <76931164+Banehowl@users.noreply.github.com>
2021-02-28 08:32:51 -0800

    Determine the  length of a List (my own)
    Daily Code for 02/28/2021. Had to make up my own today.

---
+++ DailyCode02282021.py
@@ -0,0 +1,16 @@
+# ---------------------------------------------
+# #	Daily Code	02/28/2021
+#   "Length of a List" Lesson from edabit.com
+#   Coded by: Banehowl
+# ---------------------------------------------
+
+# Determine the length of a List
+# items = "apple", "grapes", "orange"
+# itemBasket(tiems) -> 3
+
+def itemBasket(fruits):
+    solution = len(fruits)
+    return solution
+
+items = "apple", "grapes", "orange"
+print itemBasket(items)
